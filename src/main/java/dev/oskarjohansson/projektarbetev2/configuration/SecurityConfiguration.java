bdd1aca1205cfc3bf2bc9596ac1c735308842196
Oskar Johanssonlsclear <133226269+OskarEJohansson@users.noreply.github.com>
2024-06-19 14:49:06 +0200

    removed csrf filter and log as it is not needed when using JWT

--- src/main/java/dev/oskarjohansson/projektarbetev2/configuration/SecurityConfiguration.java
+++ src/main/java/dev/oskarjohansson/projektarbetev2/configuration/SecurityConfiguration.java
@@ -17,6 +17,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -54,8 +55,7 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception {
         return http
-                .csrf(Customizer.withDefaults())
-                .addFilterAfter(new CSRFLoggingFilter(), CsrfFilter.class)
+                .csrf(AbstractHttpConfigurer::disable)
                 .addFilterAfter(new AuthenticationLoggingFilter(), UsernamePasswordAuthenticationFilter.class )
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
                 .exceptionHandling(ex -> ex
